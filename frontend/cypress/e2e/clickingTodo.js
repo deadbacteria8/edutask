32595eb5738c8ef3ea1b519f51e746adcbf4ae6d
deadbacteria8 <113914918+deadbacteria8@users.noreply.github.com>
2024-05-03 15:51:57 +0200

    added json files

--- frontend/cypress/e2e/clickingTodo.js
+++ frontend/cypress/e2e/clickingTodo.js
@@ -1,67 +1,74 @@
 describe('Logging into the system', () => {
   // define variables that we need on multiple occasions
-  let uid // user id
-  let name // name of the user (firstName + ' ' + lastName)
-  let email // email of the user
-  let taskid
+  let user
+  let task
   let activeItem
-  let item
+  let notActiveItem
   before(function () {
 
     //those functions does not explicitly need to be present here but i did so because i didnt feel like i had a better place to put them
     function createUser() {
-      return cy.fixture('user.json').then((user) => {
+      return cy.fixture('user.json').then((userJson) => {
         return cy.request({
           method: 'POST',
           url: 'http://localhost:5000/users/create',
           form: true,
-          body: user
+          body: userJson
         }).then((response) => {
-          uid = response.body._id.$oid;
-          name = user.firstName + ' ' + user.lastName;
-          email = user.email;
+          user = {
+            uid : response.body._id.$oid,
+            name : userJson.firstName + ' ' + userJson.lastName,
+            email : userJson.email
+          };
           return uid;
         });
       });
     }
-    
+
     function createTask(user_id) {
-      return cy.fixture('task.json').then((task) => {
+      return cy.fixture('task.json').then((taskJson) => {
         return cy.request({
           method: 'POST',
           url: 'http://localhost:5000/tasks/create',
           form: true,
-          body: { ...task, userid: user_id }
+          body: { ...taskJson, userid: user_id }
         }).then((response) => {
-            taskid = response.body._id.$oid;
+            task = {
+              taskid : response.body._id.$oid,
+              taskTitle : taskJson.title
+            };
             return response.body._id.$oid;
         });
       });
     }
 
     function createActiveItem(taskid) {
-      return cy.fixture('item.json').then((item) => {
+      return cy.fixture('ItemActive.json').then((itemJson) => {
         return cy.request({
           method: 'POST',
           url: 'http://localhost:5000/todos/create',
           form: true,
-          body: { ...item, taskid: taskid, done: true }
+          body: { ...itemJson, taskid: taskid }
         }).then((response) => {
-            activeItem = response.body._id.$oid;
+            activeItem = {
+              id: response.body._id.$oid
+            };
             return response.body._id.$oid;
         });
       });
     }
 
     function createNotActiveItem(taskid) {
-      return cy.fixture('item.json').then((item) => {
+      return cy.fixture('ItemNotActive.json').then((itemJson) => {
         return cy.request({
           method: 'POST',
           url: 'http://localhost:5000/todos/create',
           form: true,
-          body: { ...item, taskid: taskid, done: false }
+          body: { ...itemJson, taskid: taskid}
         }).then((response) => {
-            item = response.body._id.$oid;
+            notActiveItem = {
+              id: response.body._id.$oid
+            };
             return response.body._id.$oid;
         });
       });
@@ -78,23 +85,33 @@ describe('Logging into the system', () => {
     cy.visit('/')
     cy.get('#email').type(email)
     cy.get('form').submit()
-    cy.get('h1').should('contain.text', 'Your tasks, ' + name)
+    cy.get('h1').should('contain.text', 'Your tasks, ' + user.name)
   })
 
 
 
   after(function () {
     // clean up by deleting the user from the database
+
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/todos/byid/${activeItem}`
+    })
+
+
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/todos/byid/${notActiveItem}`
+    })
+
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/tasks/byid/${taskid}`
+    })
+
     cy.request({
       method: 'DELETE',
       url: `http://localhost:5000/users/${uid}`
-    }).then((response) => {
-      cy.log(response.body)
     })
   })
 })
-
-/*fetch(`http://localhost:5000/todos/byid/${todo._id}`, {
-  method: 'delete',
-
-  'tasks/byid/<id>', methods=['GET', 'PUT', 'DELETE']*/
