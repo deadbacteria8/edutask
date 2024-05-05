848e76138da8e1ff0f402853ebe2891b7dda84bf
deadbacteria8 <113914918+deadbacteria8@users.noreply.github.com>
2024-05-05 23:48:51 +0200

    new changes

--- frontend/cypress/e2e/clickingTodo.cy.js
+++ frontend/cypress/e2e/clickingTodo.cy.js
@@ -3,6 +3,7 @@
 describe('logging in', () => {
   let user
   let task
+  let activeItem
 
   before(function () {
       cy.fixture('user.json').then((userJson) => {
@@ -24,28 +25,54 @@ describe('logging in', () => {
 
 
   beforeEach( () => {
+
+    function createTask(user_id) {
+      return cy.fixture('CreateTask.json').then((taskJson) => {
+        return cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/tasks/create',
+          form: true,
+          body: { ...taskJson, userid: user_id }
+        }).then((response) => {
+            task = {
+              id : response.body[0]._id.$oid,
+              taskTitle : taskJson.title,
+              todoTitle : response.body[0].todos[0].description
+            };
+            return response.body[0]._id.$oid;
+        });
+      });
+    }
+
+    function createActiveItem(taskid) {
+      return cy.fixture('ItemActive.json').then((itemJson) => {
+        return cy.request({
+          method: 'POST',
+          url: 'http://localhost:5000/todos/create',
+          form: true,
+          body: { ...itemJson, taskid: taskid }
+        }).then((response) => {
+            activeItem = {
+              id: response.body._id.$oid,
+              title : response.body.description
+            };
+            return response.body._id.$oid;
+        });
+      });
+    }
+
+    createTask(user.id).then((taskid) => {
+      return createActiveItem(taskid)});
+
+
     cy.visit('/')
     cy.get('#email').type(user.email)
     cy.get('form').submit()
     cy.get('h1').should('contain.text', 'Your tasks, ' + user.name)
-    cy.fixture('CreateTask.json').then((taskJson) => {
-      cy.request({
-        method: 'POST',
-        url: 'http://localhost:5000/tasks/create',
-        form: true,
-        body: { ...taskJson, userid: user.id }
-      }).then((response) => {
-          task = {
-            id : response.body[0]._id.$oid,
-            taskTitle : taskJson.title,
-            todoTitle : response.body[0].todos[0].description
-          };
-      });
-    });
   })
 
 
-  it('click checked and unchecked', () => {
+  it('click checked', () => {
     cy.get('.container-element').first().click();
     cy.contains('.todo-item .editable', task.todoTitle).parent('.todo-item')
     .as('targetTodo');
@@ -58,12 +85,15 @@ describe('logging in', () => {
 
   it('click unchecked', () => {
     cy.get('.container-element').first().click();
-    cy.contains('.todo-item .editable', task.todoTitle).parent('.todo-item')
+    cy.contains('.todo-item .editable', activeItem.title).parent('.todo-item')
     .as('targetTodo');
     cy.get('@targetTodo')
     .find('.checker')
     .click().should('have.class', 'unchecked');
+
+    
   })
+
 
 
 
@@ -72,6 +102,11 @@ describe('logging in', () => {
     cy.request({
       method: 'DELETE',
       url: `http://localhost:5000/tasks/byid/${task.id}`
+    })
+
+    cy.request({
+      method: 'DELETE',
+      url: `http://localhost:5000/todos/byid/${activeItem.id}`
     })
   });
 
@@ -84,6 +119,8 @@ describe('logging in', () => {
       method: 'DELETE',
       url: `http://localhost:5000/users/${user.id}`
     })
+
+    
 
   })
   });
